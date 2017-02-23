8d1dd8536835d2987b75f26c9e65e4fa9207c78f
Ross Crichton <rcrichton87@googlemail.com>
2017-02-23 14:00:20 +0000

    fixed typos in sql file, created seed_data.rb and pilot.rb

--- db/squad_builder.sql
+++ db/squad_builder.sql
@@ -1,11 +1,11 @@
 DROP TABLE pilots;
 
 CREATE TABLE pilots (
-  id = SERIAL4 PRIMARY KEY,
-  name = VARCHAR(255),
-  pilot_skill: INT2,
-  pilot_ability: VARCAHR(255),
-  cost: INT2,
-  ship_id: INT4
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  pilot_skill INT2,
+  pilot_ability VARCHAR(255),
+  cost INT2,
+  ship_id INT4
 )
 
