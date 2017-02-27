f8379744a88d4039c6eedf23d98c32ba43a26526
Ross Crichton <rcrichton87@googlemail.com>
2017-02-27 18:09:14 +0000

    changed adding a ship to a squad to use multiple forms and take place over one page

--- controllers/piloted_ships_controller.rb
+++ controllers/piloted_ships_controller.rb
@@ -6,6 +6,14 @@ require_relative('../models/pilot.rb')
 require_relative('../models/ship.rb')
 require_relative('../models/piloted_ship.rb') 
 
-get 'piloted_ships/new' do
+get '/piloted_ships/new' do
   erb(:"piloted_ships/new")
+end
+
+post '/piloted_ships/:squad_id' do
+  pilot = Pilot.find(params[:pilot_id].to_i)
+  ship = pilot.ship
+  piloted_ship = PilotedShip.new({ 'pilot_id' => pilot.id, 'ship_id' => ship.id, 'squad_id' => params[:squad_id].to_i })
+  piloted_ship.save
+  redirect to "/squads/#{params[:squad_id].to_i}"
 end
