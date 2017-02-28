e1b7d786660769fdf377113a97dfcb4a672e1c45
Ross Crichton <rcrichton87@googlemail.com>
2017-02-28 09:50:17 +0000

    added save method to upgrade

--- models/upgrade.rb
+++ models/upgrade.rb
@@ -1,3 +1,5 @@
+require_relative('../db/sql_runner.rb')
+
 class Upgrade
 
   attr_reader :id
@@ -9,6 +11,12 @@ class Upgrade
     type = options['type']
     cost = options['cost'].to_i
     effect = options['effect'] 
+  end
+
+  def save
+    sql = "INSERT INTO upgrades (name, type, cost, effect) VALUES (#{@name}, #{@type}, #{@cost}, #{@effect} RETURNING *;"
+    result = SqlRunner.run(sql).first
+    @id = result['id'].to_i
   end
 
 end
