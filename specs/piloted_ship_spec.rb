07ec13c0e45c6e4cad3a2557ca578b77c2d6d523
Ross Crichton <rcrichton87@googlemail.com>
2017-03-26 14:54:08 +0100

    can get the id for a piloted_ship

---
+++ specs/piloted_ship_spec.rb
@@ -0,0 +1,52 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../models/pilot.rb')
+require_relative('../models/ship.rb')
+require_relative('../models/squad.rb')
+require_relative('../models/piloted_ship.rb')
+
+
+class TestPilotedShip < MiniTest::Test
+
+  def setup
+    @ship = Ship.new({
+      'id' => '1',
+      'name' => 'T-65 X-Wing',
+      'stats' => '3, 2, 2, 3',
+      'upgrades' => 'astromech, torpedo, modification',
+      'manoeuvres' => '/images/x_wing_manoeuvres.png',
+      'cost' => 21,
+      'faction' => 'rebel'
+    })
+  
+    @pilot = Pilot.new({
+      'id' => '4',
+      'name' => 'Luke Skywalker',
+      'pilot_skill' => '8',
+      'pilot_ability' => 'When defending, you may change one focus result to an evade result.',
+      'cost' => '7',
+      'ship_id' => @ship.id
+    })
+
+    @squad = Squad.new({
+      'name' => 'Rogue Squadron', 
+      'faction' => 'rebel', 
+      'id' => '10'
+    })
+
+    @piloted_ship = PilotedShip.new({
+      'id' => '2',
+      'pilot_id' => @pilot.id, 
+      'ship_id' => @ship.id, 
+      'squad_id' => @squad.id,
+    })
+
+  end
+
+  def test_id
+    expected = 2
+    actual = @piloted_ship.id
+    assert_equal(expected, actual)
+  end
+
+end
